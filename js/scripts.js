e2be05954084654884e134d99e010d646f204d67
Damjan Roskar <damjan.roskar@bearingpoint.com>
2018-10-18 13:13:12 +0200

    Dynamically calculate age

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,13 @@ $("#junk_button").click(function(){
     window.location.href = "junk.html"
 });
 
-
+$("#myAge").html("\<span\>Age\<\/span\>" + getMyAge());
 });
+
+
+function getMyAge() {
+    var myBirthday = new Date(1991, 11, 24, 12);
+    var ageDifference = Date.now() - myBirthday.getTime();
+    var ageDate = new Date(ageDifference);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
