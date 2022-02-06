1a94a6e292901f5c53c0619217a92f4d0b170f4d
dRoskar <damjan.roskar.s@gmail.com>
2022-02-06 17:29:28 +0100

    KC basic movement

---
+++ kiss-cakic/script/gameScene.js
@@ -0,0 +1,52 @@
+class Game extends Phaser.Scene {
+
+    constructor () {
+        super('Game');
+        this.keys;
+        this.ship;
+    }
+
+    init() {
+    }
+
+    preload() {
+        this.load.image('background', 'assets/bg.jpg');
+        this.load.image('cakic', 'assets/cakic.jpg');
+        this.load.image('ship', 'assets/ship.png');
+    }
+
+    create(data) {
+        this.add.image(400, 300, 'background');
+        this.add.image(400, 300, 'cakic');
+
+        this.ship = this.physics.add.staticImage(400, 450, 'ship');
+
+        // input
+        this.keys = this.input.keyboard.addKeys('SPACE,W,A,D,LEFT,RIGHT,UP');
+
+        // input
+        this.input.keyboard.on('keydown-ESC', function(event) {
+            console.log('ESC');
+            event.stopPropagation();
+            this.scene.start('Title');
+        }, this);
+        
+    }
+
+    update(time, delta) {
+        if(this.keys.SPACE.isDown || this.keys.W.isDown || this.keys.UP.isDown) {
+            console.log('pew pew');
+        }
+
+        if(this.keys.A.isDown || this.keys.LEFT.isDown) {
+            this.ship.x = this.ship.x - 5;
+        }
+
+        
+        if(this.keys.D.isDown || this.keys.RIGHT.isDown) {
+            this.ship.x = this.ship.x + 5;
+        }
+    }
+}
+
+export default Game;
---
+++ kiss-cakic/script/kc.js
@@ -0,0 +1,24 @@
+import Title from './titleScene.js';
+import Game from './gameScene.js';
+import End from './endScene.js';
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    disableContextMenu: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [
+        Title,
+        Game,
+        End
+    ]
+};
+
+var game = new Phaser.Game(config);
