497498df5b1447eeb4212f07d24c81b04677c28d
dRoskar <damjan.roskar.s@gmail.com>
2022-02-09 23:05:22 +0100

    KC basic shooting

--- kiss-cakic/script/gameScene.js
+++ kiss-cakic/script/gameScene.js
@@ -4,6 +4,8 @@ class Game extends Phaser.Scene {
         super('Game');
         this.keys;
         this.ship;
+        this.shooting;
+        this.shot;
     }
 
     init() {
@@ -13,13 +15,18 @@ class Game extends Phaser.Scene {
         this.load.image('background', 'assets/bg.jpg');
         this.load.image('cakic', 'assets/cakic.jpg');
         this.load.image('ship', 'assets/ship.png');
+        this.load.image('shot', 'assets/ray.png');
     }
 
     create(data) {
+        this.shooting = false;
+
         this.add.image(400, 300, 'background');
         this.add.image(400, 300, 'cakic');
 
         this.ship = this.physics.add.staticImage(400, 575, 'ship');
+        this.shot = this.physics.add.staticImage(400, 300, 'shot').setVisible(false);
+        // this.shots = this.physics.add.staticGroup();
 
         // input
         this.keys = this.input.keyboard.addKeys('SPACE,W,A,D,LEFT,RIGHT,UP');
@@ -33,8 +40,11 @@ class Game extends Phaser.Scene {
     }
 
     update(time, delta) {
-        if(this.keys.SPACE.isDown || this.keys.W.isDown || this.keys.UP.isDown) {
+        if((this.keys.SPACE.isDown || this.keys.W.isDown || this.keys.UP.isDown) && !this.shooting) {
             console.log('pew pew');
+            this.shot.setPosition(this.ship.x, this.ship.y - 15);
+            this.shot.setVisible(true);
+            this.shooting = true;
         }
 
         if(this.keys.A.isDown || this.keys.LEFT.isDown) {
@@ -46,6 +56,15 @@ class Game extends Phaser.Scene {
         if(this.keys.D.isDown || this.keys.RIGHT.isDown) {
             if(this.ship.x <= 760) {
                 this.ship.x = this.ship.x + 4;
+            }
+        }
+
+        if(this.shooting) {
+            this.shot.y = this.shot.y - 10;
+
+            if(this.shot.y <= 0) {
+                this.shot.setVisible(false);
+                this.shooting = false;
             }
         }
     }
